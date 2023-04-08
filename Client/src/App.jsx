51a0f2133570b03994b02353f49abcfb3b9f035b
Muhammed Zulfiker A <muhammedzulfikerindia@gmail.com>
2023-04-08 23:41:46 +0530

    Finish basic layout and Routers

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,87 @@
-import { useState } from 'react'
+import { Children, useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+import Home from './pages/Home/Home';
+import Gigs from './pages/gigs/Gigs';
+import Gig from './pages/gig/Gig';
+import Login from './pages/login/login';
+import Register from './pages/register/register';
+import Messages from './pages/messages/messages';
+import Message from './pages/message/message';
+import MyGigs from './pages/MyGIgs/MyGigs';
+import Orders from './pages/Orders/Orders';
+import Add from './pages/Add/Add';
+
+const Layout =()=>{
+  return <div className="app">
+    <Navbar />
+    <Outlet />
+    <Footer/>
+  </div>
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children : [
+      {
+        path : "/",
+        element : <Home />
+      },
+      {
+        path : "/gigs",
+        element : <Gigs />
+      },
+      {
+        path : "/gig/:id",
+        element : <Gig />
+      },
+      {
+        path : "/login",
+        element : <Login />
+      },
+      {
+        path : "/register",
+        element : <Register />
+      },
+      {
+        path : "/messages",
+        element : <Messages />
+      },
+      {
+        path : "/message/:id",
+        element : <Message />
+      },
+      {
+        path : "/mygigs",
+        element : <MyGigs />
+      },
+      {
+        path : "/orders",
+        element : <Orders />
+      },
+      {
+        path : "/addnew",
+        element : <Add />
+      },
+      
+    ]
+  },
+]);
 
 function App() {
 
   return (
    <div>
-    <Navbar />
+    <RouterProvider router={router} />
    </div>
   )
 }
--- Client/src/components/Navbar/Navbar.jsx
+++ Client/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,10 @@
 import React , {useState, useEffect} from 'react'
 import "./Navbar.scss"
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   const [showMenu , setshowMenu] = useState(false)
+  const [userMenu , setUserMenu] = useState(false)
   const menuTrigger =()=>{
     window.scrollY > 0 ? setshowMenu(true) : setshowMenu(false)
   }
@@ -12,23 +14,41 @@ const Navbar = () => {
     return ()=>{
       window.removeEventListener('scroll',menuTrigger)
     }
-  })
+  },[])
+  const currentUser = {
+    username : "Zulfiker",
+    isSeller : true,
+    imageUrl : `https://ui-avatars.com/api/?name=`
+  }
   return (
     <div className={showMenu ? 'navbar active' : 'navbar' }>
       <div className="container">
         <div className="logo">
-          <span className="text">
-            fiverr
-          </span>
+          <Link to="/" className="text">
+              fiverr
+          </Link>
           <span className="dot">.</span>
         </div>
         <div className="links">
           <span>Fiver Business</span>
           <span>Explore</span>
           <span>English</span>
-          <span>Become a Seller</span>
-          <span>Sign In</span>
-          <button className={showMenu && 'active'}>Join</button>
+          {!currentUser?.isSeller && <span>Become a Seller</span>}
+          {!currentUser ? <button className={showMenu && 'active'}>Join</button> : 
+            <>
+              <img className='profile_pic' src={currentUser.imageUrl+currentUser.username+'&rounded=true'} alt="userlogo"/>
+              <span onClick={()=> setUserMenu(!userMenu)}>{currentUser.username}</span>
+            </>
+          }
+          {
+            currentUser && userMenu && <div className="usermenu">
+              <Link to="/gigs">Gigs</Link>
+              <Link to="addnew">Add a new Gig</Link>
+              <Link to="orders">Orders</Link>
+              <Link to="messages">Messages</Link>
+              <Link>Logout</Link>
+            </div>
+          }
         </div>
       </div>
       { showMenu &&
