899a7ad9cd4fb6d3ab39b7e082f8a7974b635c3b
Muhammed Zulfiker A <muhammedzulfikerindia@gmail.com>
2023-04-16 12:02:49 +0530

    features part completed

--- Client/src/pages/Home/Home.jsx
+++ Client/src/pages/Home/Home.jsx
@@ -19,6 +19,40 @@ const Home = () => {
             })
         }
       </Slide>
+      <div className="features">
+        <div className="details">
+          <h1 className="features__heading">A whole world of freelance talent at your fingertips</h1>
+          <div className="points">
+            <div className="features__title">
+              <img src="./img/check.png" alt="checktick" />
+              The best for every budget
+            </div>
+            <p className="features__desc">Find high-quality services at every price point. No hourly rates, just project-based pricing.</p>
+            <div className="features__title">
+            <img src="./img/check.png" alt="checktick" />Quality work done quickly
+            </div>
+            <p className="features__desc">
+            Find the right freelancer to begin working on your project within minutes.
+            </p>
+            <div className="features__title">
+            <img src="./img/check.png" alt="checktick" />Protected payments, every time
+            </div>
+            <p className="features__desc">
+            Always know what you'll pay upfront. Your payment isn't released until you approve the work.
+            </p>
+            <div className="features__title">
+            <img src="./img/check.png" alt="checktick" />24/7 support
+            </div>
+            <p className="features__desc">
+            Questions? Our round-the-clock support team is available to help anytime, anywhere.
+            </p>
+          </div>
+
+        </div>
+          <div className="features_video">
+            <video controls src='./img/AdVideo.mp4' />
+          </div>
+      </div>
     </div>
   )
 }
